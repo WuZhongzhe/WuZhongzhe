497fadd9b88864ac9d896b6c896394ff1c66e891
WuZhongzhe <1405601760@qq.com>
2026-09-19 00:46:03 +0800

    实现表达式随机生成、求值与题目答案文件输出

---
+++ 生成四则运算题目/src/arithmetic/Expression.java
@@ -0,0 +1,114 @@
+package arithmetic;
+
+import java.util.Random;
+
+/**
+ * 算术表达式，用二叉树存：叶子结点是数字，其它结点是运算符。
+ */
+public class Expression {
+
+    /** 表达式的结点 */
+    public static class Node {
+        public Fraction value;   // 这棵子树的运算结果
+        public char op;          // 叶子结点为 0
+        public Node left;
+        public Node right;
+
+        public Node(Fraction value) {
+            this.value = value;
+            this.op = 0;
+        }
+
+        public Node(Fraction value, char op, Node left, Node right) {
+            this.value = value;
+            this.op = op;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static final char[] OPS = {'+', '-', '×', '÷'};
+
+    /**
+     * 随机生成一个含有 opCount 个运算符的表达式，数值范围由 range 控制。
+     * 中途不满足要求（减法出负数、除以 0 等）就返回 null，让调用方重试。
+     */
+    public static Node generate(Random rnd, int opCount, int range) {
+        if (opCount == 0) {
+            return new Node(randomNumber(rnd, range));
+        }
+        char op = OPS[rnd.nextInt(OPS.length)];
+        int leftCount = rnd.nextInt(opCount);
+        int rightCount = opCount - 1 - leftCount;
+        Node left = generate(rnd, leftCount, range);
+        if (left == null) {
+            return null;
+        }
+        Node right = generate(rnd, rightCount, range);
+        if (right == null) {
+            return null;
+        }
+        // 右子树的运算符和父结点相同的话括号是多余的，重来
+        if (right.op == op) {
+            return null;
+        }
+        Fraction value;
+        if (op == '+') {
+            value = left.value.add(right.value);
+        } else if (op == '-') {
+            if (left.value.compareTo(right.value) < 0) {
+                return null;
+            }
+            value = left.value.sub(right.value);
+        } else if (op == '×') {
+            value = left.value.mul(right.value);
+        } else {
+            if (right.value.isZero()) {
+                return null;
+            }
+            value = left.value.div(right.value);
+        }
+        // 结果太大就不像小学题了
+        if (value.toDouble() > 10000 || value.getDen() > 10000) {
+            return null;
+        }
+        return new Node(value, op, left, right);
+    }
+
+    /** 随机生成一个自然数或者真分数，都小于 range */
+    private static Fraction randomNumber(Random rnd, int range) {
+        if (range <= 1) {
+            return new Fraction(0);
+        }
+        if (range <= 2 || rnd.nextInt(2) == 0) {
+            return new Fraction(rnd.nextInt(range));
+        }
+        int den = 2 + rnd.nextInt(range - 2);
+        int num = 1 + rnd.nextInt(den - 1);
+        return new Fraction(num, den);
+    }
+
+    /** 转成题目字符串，数字和运算符之间用空格隔开 */
+    public static String toString(Node n) {
+        if (n.op == 0) {
+            return n.value.toString();
+        }
+        int level = priority(n.op);
+        return child(n.left, level, false) + " " + n.op + " " + child(n.right, level, true);
+    }
+
+    private static String child(Node c, int parentLevel, boolean isRight) {
+        if (c.op == 0) {
+            return c.value.toString();
+        }
+        int level = priority(c.op);
+        if (level < parentLevel || (isRight && level == parentLevel)) {
+            return "(" + toString(c) + ")";
+        }
+        return toString(c);
+    }
+
+    private static int priority(char op) {
+        return (op == '×' || op == '÷') ? 2 : 1;
+    }
+}
--- 生成四则运算题目/src/arithmetic/Main.java
+++ 生成四则运算题目/src/arithmetic/Main.java
@@ -1,5 +1,9 @@
 package arithmetic;
 
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Random;
+
 /**
  * 程序入口。
  * 生成题目：Myapp.exe -n 10 -r 10
@@ -26,8 +30,36 @@ public class Main {
         if (n < 0) {
             n = 10;
         }
-        System.out.println("参数解析完成：题目个数 " + n + "，数值范围 " + r);
-        System.out.println("生成题目的部分还没写，下一次提交补上");
+        generate(n, r);
+    }
+
+    private static void generate(int n, int r) {
+        Random rnd = new Random();
+        String exercises = "";
+        String answers = "";
+        for (int i = 0; i < n; i++) {
+            Expression.Node node = null;
+            // 不合格的表达式会返回 null，重试直到拿到一个能用的
+            while (node == null) {
+                int opCount = 1 + rnd.nextInt(3);
+                node = Expression.generate(rnd, opCount, r);
+            }
+            exercises = exercises + (i + 1) + ". " + Expression.toString(node) + " =" + "\r\n";
+            answers = answers + (i + 1) + ". " + node.value + "\r\n";
+        }
+        write("Exercises.txt", exercises);
+        write("Answers.txt", answers);
+        System.out.println("生成了 " + n + " 道题目，已写入 Exercises.txt 和 Answers.txt");
+    }
+
+    private static void write(String fileName, String content) {
+        try {
+            OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8");
+            w.write(content);
+            w.close();
+        } catch (Exception e) {
+            System.out.println("写文件 " + fileName + " 失败：" + e.getMessage());
+        }
     }
 
     private static void printHelp() {
